52c7a03042466c8a8d1d26272a3487634035df14
Nanne Hemstra <nanne.hemstra@dgbes.com>
2015-01-06 12:45:51 +0000

    Removed mSeisPreLoadMgrSel2DHelpID

--- od_userdoc/Project/Advanced/CSH/od_helpids.h
+++ od_userdoc/Project/Advanced/CSH/od_helpids.h
@@ -164,7 +164,6 @@ external/doc_csh instance of it.
 #define		mSeisIOSimpleImpHelpID			        0x10300011
 #define		mSeisIOSimpleExpHelpID			        0x10300012
 #define		mSeisPreLoadMgrHelpID			        0x10300013
-#define		mSeisPreLoadMgrSel2DHelpID			0x10300014
 #define		mSEGYExpdoWorkHelpID			        0x10300015
 #define		mDZTImporterHelpID				0x10300016
 #define		mSeisFmtScaleDlgHelpID			        0x10300017
