317cfafdada98b67ee7c488734044842f8358000
Nanne Hemstra <hemstra@users.noreply.github.com>
2016-09-15 10:17:02 -0500

    Added helpid for new Pseudo Relief attribute

--- od_userdoc/Project/Advanced/CSH/od_helpids_orig.h
+++ od_userdoc/Project/Advanced/CSH/od_helpids_orig.h
@@ -147,6 +147,7 @@ external/doc_csh instance of it.
 #define         mGrubbsFilterAttribHelpID                       0x10100107
 #define         mVolProcAttribHelpID                            0x10100108
 #define         mEventFreqAttribHelpID                          0x10100109
+#define		mReliefAttribHelpID				0x10100110
 // Attributes Selection 01
 #define		mAttribDescSetEdHelpID			        0x10101000
 #define		mAttrSelDlgNo_NNHelpID			        0x10101001
