3d4923abcde681ba7075bb1ed389b2b5545391b2
arnauddgbes <arnaud.huck@dgbes.com>
2018-03-19 12:25:36 +0100

    added help id for CEEMD attribute

--- od_userdoc/Project/Advanced/CSH/od_helpids_orig.h
+++ od_userdoc/Project/Advanced/CSH/od_helpids_orig.h
@@ -146,6 +146,7 @@ external/doc_csh instance of it.
 #define         mVolProcAttribHelpID                            0x10100108
 #define         mEventFreqAttribHelpID                          0x10100109
 #define		mReliefAttribHelpID				0x10100110
+#define		mCEEMDAttribHelpID				0x10100111
 // Attributes Selection 01
 #define		mAttribDescSetEdHelpID			        0x10101000
 #define		mAttrSelDlgNo_NNHelpID			        0x10101001
