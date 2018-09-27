6c13e4d35ae5d5488a943fe989e21d18feb3a49a
arnauddgbes <arnaud.huck@dgbes.com>
2018-09-27 09:48:48 +0200

    od6.2: added two missing helpIDs

--- od_userdoc/Project/Advanced/CSH/od_helpids.h
+++ od_userdoc/Project/Advanced/CSH/od_helpids.h
@@ -188,6 +188,7 @@ external/doc_csh instance of it.
 #define		mSeisIOSimpleImpHelpID			        0x10300011
 #define		mSeisIOSimpleExpHelpID			        0x10300012
 #define		mSeisPreLoadMgrHelpID			        0x10300013
+#define		mSeisPreLoad2D3DDataHelpID			0x10300014
 #define		mDZTImporterHelpID				0x10300016
 #define		mSeisFmtScaleDlgHelpID			        0x10300017
 #define		mSEGYFileManipHelpID			        0x10300019
